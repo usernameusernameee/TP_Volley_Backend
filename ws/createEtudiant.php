f92123bb94cfb269ab42e685e5b3683c7febeb44
mohamed <nomn28097@gmail.com>
2025-04-11 00:21:11 +0100

    second commit

---
+++ ws/createEtudiant.php
@@ -0,0 +1,22 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+include_once '../racine.php';
+include_once RACINE.'/service/EtudiantService.php';
+create();
+}
+function create(){
+extract($_POST);
+$es = new EtudiantService();
+$es->create(new Etudiant(1, $nom, $prenom, $ville, $sexe));
+}
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){ chdir(".."); include_once 'service/EtudiantService.php';
+create();
+}
+function create(){
+extract($_POST);
+$es = new EtudiantService();
+//insertion d’un étudiant
+$es->create(new Etudiant(1, $nom, $prenom, $ville, $sexe));
+//chargement de la liste des étudiants sous format json header('Content-type: application/json'); echo json_encode($es->findAllApi());
+}
